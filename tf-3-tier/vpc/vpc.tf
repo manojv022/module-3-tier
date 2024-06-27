aecc33310495b78439b546325a9968804ead9151
manojv022 <167419795+manojv022@users.noreply.github.com>
2024-06-27 23:00:24 +0530

    Create vpc.tf

---
+++ tf-3-tier/vpc/vpc.tf
@@ -0,0 +1,41 @@
+variable "cidr_block" {
+    type = string
+  
+}
+
+variable "vpc_name" {
+    type = string
+  
+}
+
+variable "subnet_cidr_block" {
+    type = list(string)
+  
+}
+
+variable "subnet_az" {
+  type = list(string)
+}
+
+variable "public_ip" {
+  type = list(bool)
+}
+
+variable "subnet_name" {
+    type = list(string)
+  
+}
+
+variable "igw_name" {
+    type = string
+  
+}
+
+variable "sg_name" {
+    type = string
+  
+}
+
+variable "ports" {
+    type = list(number)  
+}
