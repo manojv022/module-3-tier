78a9b048d706c3e37d756316f6ecbe2ae42f494c
manojv022 <167419795+manojv022@users.noreply.github.com>
2024-06-27 22:59:40 +0530

    Create var.tf

---
+++ tf-3-tier/rds/var.tf
@@ -0,0 +1,64 @@
+variable "subnet_2" {
+    type = string 
+  
+}
+
+variable "subnet_3" {
+    type = string 
+  
+}
+
+variable "sg_id" {
+    type = string
+  
+}
+
+variable "rds_subnet_group" {
+    type = string
+  
+}
+
+variable "allow_storage" {
+    type = number
+  
+}
+
+variable "db_name" {
+    type = string
+  
+}
+
+variable "engine" {
+    type = string
+  
+}
+
+variable "engine_version" {
+    type = string
+  
+}
+
+variable "admin" {
+    type = string
+  
+}
+
+variable "passwd" {
+    type = string
+  
+}
+
+variable "instance_class" {
+    type = string
+  
+}
+
+variable "snapshot" {
+    type = bool
+  
+}
+
+variable "rds_name" {
+    type = string
+  
+}
