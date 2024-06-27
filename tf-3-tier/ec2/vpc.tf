2af13c7663c8855792dc7bc9f62360a2e3a260e7
manojv022 <167419795+manojv022@users.noreply.github.com>
2024-06-27 22:57:59 +0530

    Update vpc.tf

--- tf-3-tier/ec2/vpc.tf
+++ tf-3-tier/ec2/vpc.tf
@@ -1,1 +1,44 @@
 
+variable "subnet_1" {
+    type = string 
+  
+}
+
+variable "subnet_2" {
+    type = string 
+  
+}
+
+variable "subnet_3" {
+    type = string 
+  
+}
+
+variable "sg_id" {
+    type = string
+  
+}
+
+variable "private_ip" {
+    type = list(string)
+}
+
+variable "ami_id" {
+    type = string
+  
+}
+
+variable "instance_type" {
+    type = string
+  
+}
+
+variable "key_name" {
+    type = string
+  
+}
+
+variable "instance_name" {
+    type = list(string)
+  
+}
